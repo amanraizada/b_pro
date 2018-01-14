e0d1ee268ae7a21fa5452b97cbca91842abf5226
ahmad-taha2 <ahmadtahaco@gmail.com>
2018-01-14 19:24:53 +0530

    Update and rename domin.py to app.py

--- app.py
+++ app.py
@@ -10,12 +10,14 @@ btc = "3MUT2imvKa2V4BZ6cBGAfZFeQ5PS9kopdk"
 
 
 chrome_options = Options()  
+chrome_options.binary_location = r"/app/.apt/usr/bin/google-chrome"
 # chrome_options.add_argument("--headless")
 
 print("------------------------------------------------------\nDOMINIUMPRO.com BOT || VERSION 1.0 \n------------------------------------------------------\nBY - AHMAD TAHA || CONTACT - ahmadtahaco@gmail.com\n======================================================\n")
 print ("\n>> Opening Browser")
 
-driver = webdriver.Chrome(executable_path="chromedriver.exe",chrome_options=chrome_options)
+driver = webdriver.Chrome(chrome_options=chrome_options)
+#driver = webdriver.Chrome(executable_path="chromedriver.exe",chrome_options=chrome_options)
 driver.maximize_window()
 
 driver.get("http://www.dominiumpro.com/index.php")
@@ -32,14 +34,16 @@ driver.switch_to_frame(driver.find_element_by_xpath('/html/body/div/div[2]/div[2
 time.sleep(10)
 captcha = driver.find_element_by_xpath('//*[@id="mining-button-text"]')
 captcha.click()
-
+t = 0
 while True:
+    t+=1
     time.sleep(20)
 
     hs = driver.find_element_by_xpath('//*[@id="hashes-per-second"]')
     hs = hs.get_attribute("textContent")
     if "0.0" in hs:
-        crash
+        if t == 5:
+            crash
     print(">> Amount : " + hs)
 
 print(">> Current Claimed : " + str("lol") + " || Total Claimed : " + str("lol"))
